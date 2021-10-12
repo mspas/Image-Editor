eb73dfbdd0357ab8750ecd4bc645e121fdf6ba35
Marcin Spasinski <marcin7789@gmail.com>
2021-10-12 10:13:43 +0200

    Updates

---
+++ precache-manifest.808ad79a4526cb02f5a47bf4575a69db.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cdd4f5d0042f5db8799b49d26af689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc39c98d872421c4546",
+    "url": "/static/css/main.6c8ab027.chunk.css"
+  },
+  {
+    "revision": "163d49705f4649f0165e",
+    "url": "/static/js/2.5cf1c828.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/static/js/2.5cf1c828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c07f883d50a841735bf4419a3573e21",
+    "url": "/static/js/editor.worker.c7104402.worker.js"
+  },
+  {
+    "revision": "8dc39c98d872421c4546",
+    "url": "/static/js/main.743370a9.chunk.js"
+  },
+  {
+    "revision": "bca185d0c630a54fb549",
+    "url": "/static/js/runtime-main.bb8087b5.js"
+  },
+  {
+    "revision": "e3e87b011a2b82e378fbae5e8f92246e",
+    "url": "/static/media/1280x720.e3e87b01.jpg"
+  },
+  {
+    "revision": "79173b99b954caf8ff9670bd4b6efe2c",
+    "url": "/static/media/1920x1080.79173b99.jpg"
+  },
+  {
+    "revision": "3f407475ee99a1dea7979069238a9049",
+    "url": "/static/media/200x200.3f407475.jpg"
+  },
+  {
+    "revision": "0826051e15d5e441eb16499f0ca9ee82",
+    "url": "/static/media/3840x2160.0826051e.jpg"
+  },
+  {
+    "revision": "3c600a4697769ab411f5bfef74fceb8e",
+    "url": "/static/media/4500x3000.3c600a46.jpg"
+  },
+  {
+    "revision": "516926c6baf8e17754e89eb4e2dcee7a",
+    "url": "/static/media/5463x3418.516926c6.jpg"
+  },
+  {
+    "revision": "9207a11899722f54473a21c825fc0985",
+    "url": "/static/media/720x480.9207a118.jpg"
+  },
+  {
+    "revision": "489566b568b98d1af2957941e884f2c4",
+    "url": "/static/media/7680x4320.489566b5.jpg"
+  },
+  {
+    "revision": "19667022a83fa9705fa80a7ce4fe43ed",
+    "url": "/static/media/video2.19667022.mp4"
+  }
+]);
